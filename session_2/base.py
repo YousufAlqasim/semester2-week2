d3d6a3ee93b3f7212794f7a4cbb7e12c9c2bf6ec
Yousuf Alqasim <zwgf0500@leeds.ac.uk>
2026-02-07 22:45:41 +0000

    s

--- session_2/base.py
+++ session_2/base.py
@@ -10,14 +10,67 @@ def get_connection(db_path="orders.db"):
     """
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
+
     return conn
 
 def main():
 
     db = get_connection()
+    
 
     db.close()
 
 
 if __name__=="__main__":
     main()
+
+def list_categories():
+    query = """SELECT category FROM products"""
+
+    db = get_connection()
+    cursor = db.cursor()
+
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    db.close()
+
+    # Convert rows to normal Python list
+    categories = [row["category"] for row in results]
+    return categories
+
+def customer_count():
+    query = """SELECT COUNT(first_name) FROM customers"""
+
+    db = get_connection()
+    cursor = db.cursor()
+
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    db.close()
+    count = results[0][0]
+    return count
+    
+def order_customer(customer_email):
+    query ="""SELECT o.order_id,
+    o.customer_id,
+    o.order_date,
+    o.status,
+    o.total_amount
+    FROM orders o
+    JOIN customers c ON c.customer_id = o.customer_id
+    WHERE c.email = %s """
+
+    db = get_connection()
+    cursor = db.cursor()
+
+    cursor.execute(query,(customer_email))
+    results = cursor.fetchall()
+
+    db.close()
+    x = input("Email Address?")
+
+    print(results)
+
+order_customer("barkermohammad@example.com")
