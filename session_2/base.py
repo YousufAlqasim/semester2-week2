068ef9be2a84460f10265dc4762b1639d2e77a74
Yousuf Alqasim <zwgf0500@leeds.ac.uk>
2026-02-08 00:44:40 +0000

    s

--- session_2/base.py
+++ session_2/base.py
@@ -60,17 +60,46 @@ def order_customer(customer_email):
     o.total_amount
     FROM orders o
     JOIN customers c ON c.customer_id = o.customer_id
-    WHERE c.email = %s """
+    WHERE c.email = ? """
 
     db = get_connection()
     cursor = db.cursor()
 
-    cursor.execute(query,(customer_email))
+    cursor.execute(query,(customer_email,))
     results = cursor.fetchall()
 
     db.close()
-    x = input("Email Address?")
+    categories = [[row["order_id"], row["order_date"], row["status"], row["total_amount"]] for row in results]
 
-    print(results)
+    return categories
 
-order_customer("barkermohammad@example.com")
+def product_under_2():
+    query ="""SELECT product_id,name,category,price FROM products WHERE price<2; """
+
+    db = get_connection()
+    cursor = db.cursor()
+
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    db.close()
+
+    categories = [[row["product_id"], row["name"], row["category"], row["price"]] for row in results]
+    return categories
+
+def total_spent():
+    query ="""SELECT
+    c.customer_id,
+    c.first_name,
+    SUM(oi.unit_price) AS total
+    FROM customers c
+    JOIN orders o ON c.customer_id = o.customer_id
+    JOIN order_items oi ON o.order_id = oi.order_id"""
+
+    db = get_connection()
+    cursor = db.cursor()
+
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    db.close()
